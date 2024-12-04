ef6266783dc646a2e428ea682b036850dc547999
jhandry naula <jjnaula3@utpl,edu,ec>
2024-12-04 11:38:44 -0500

    cambios4

---
+++ ManejoCicloFor/src/paquete05/ImprimirNombre.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete05;
+
+/**
+ *
+ * @author USUARIO
+ */
+    public class ImprimirNombre {
+    public static void main(String[] args) {
+        // El nombre que quieres imprimir
+        String nombre = "TuNombre";
+        
+        // Imprimir el nombre
+        System.out.println("El nombre es: " + nombre);
+    }
+}
+
+    
+
