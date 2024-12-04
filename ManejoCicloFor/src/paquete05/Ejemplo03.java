fc6a19a55de5fc8aa01201352f3ae71ea3a4105e
Andrea <criolloandrea20@hotmail.com>
2024-12-04 12:06:28 -0500

    Ejemplo

---
+++ ManejoCicloFor/src/paquete05/Ejemplo03.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete05;
+
+import paquete01.*;
+
+/**
+ *
+ * @author reroes
+ */
+public class Ejemplo03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int num = 0;
+        int limite = 1;
+        int multiplicacion;
+        
+        System.out.println("Ingrese el numero que desee multiplicar");
+        
+        while (num <= limite) {
+            multiplicacion = num * limite; // 0 + 10
+                                                   // 10 + 12
+                                                   // 22 + 14
+        }
+        
+        System.out.printf("%d\n", num);
+        
+    }
+    
+}
